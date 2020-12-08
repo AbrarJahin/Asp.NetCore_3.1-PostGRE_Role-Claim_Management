6c408fff04f5d465b3101dd1c871c4b7281bead7
Abrar Jahin <abrarjahin@live.com>
2020-12-08 17:49:44 +0600

    Models added for application

---
+++ Data/Enums/ELeaveType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StartupProject_Asp.NetCore_PostGRE.Data.Enums
+{
+    public enum ELeaveType
+    {
+        [Display(Name = "Annual Leave")]
+        Annual = 0,
+        [Display(Name = "Home Leave")]
+        Home,
+        [Display(Name = "Special Leave")]
+        Special,
+        [Display(Name = "Maternity Leave")]
+        Maternity,
+        [Display(Name = "Sick Leave")]
+        Sick
+    }
+}
---
+++ Data/Models/AppData/XmlFile.cs
@@ -0,0 +1,44 @@
+﻿using StartupProject_Asp.NetCore_PostGRE.Data.Enums;
+using StartupProject_Asp.NetCore_PostGRE.Data.Models.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace StartupProject_Asp.NetCore_PostGRE.Data.Models.AppData
+{
+    public class XmlFile : BaseModel
+    {
+        public XmlFile() { }
+        public XmlFile(string xmlString, string fileName, long dbEntryId, ETableName tableName)
+        {
+            FileContent = xmlString;
+            DbEntryId = dbEntryId;
+            FileRealName = fileName;
+            TableName = tableName;
+            //DownloadUploadTokens = new List<DownloadUploadToken>();
+        }
+
+        [IgnoreDataMember]
+        [ConcurrencyCheck]  //2 file should not be same
+        [Column("FileContent", TypeName = "text"), Required(ErrorMessage = "File Content should be given"), MinLength(5), Display(Name = "File Content", Prompt = "Please Give File Content")]
+        public string FileContent { get; set; }
+        [Column("FileRealName"), Required(ErrorMessage = "Real Name should be given"), MinLength(5), MaxLength(32767), Display(Name = "File Real Name", Prompt = "Please Give File Real Name")]
+        public string FileRealName { get; set; }
+
+        public ETableName TableName { get; set; } = ETableName.XmlFile;
+        public long DbEntryId { get; set; }
+        public bool IsAlreadyUsed { get; set; } = false;
+
+        [IgnoreDataMember]
+        [Column("SignerId"), Display(Name = "Signer Id", Prompt = "Please Give Signer Id")]
+        public long? SignerId { get; set; }
+        [ForeignKey("SignerId"), Display(Name = "Previous Signed/Unsigned File", Prompt = "Please Select Previous File")]
+        public virtual User Signer { get; set; }
+        [Column("PreviousFileId"), Display(Name = "Previous Signed/Unsigned File", Prompt = "Please select Previous File")]
+        public long? PreviousFileId { get; set; }
+        [ForeignKey("PreviousFileId"), Display(Name = "Previous Signed/Unsigned File", Prompt = "Please Select Previous File")]
+        public virtual XmlFile PreviousSignedFile { get; set; }
+
+        //public ICollection<DownloadUploadToken> DownloadUploadTokens { get; set; }
+    }
+}
